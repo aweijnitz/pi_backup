dcb7ed0a7e935c6b6a8c1948a11a1a7e919742f9
cornasdf <erinrjones@gmail.com>
2014-02-13 15:02:48 +0000

    Update backup.sh
    # 2014-Feb-12
    # Merged in comments from http://www.raspberrypi.org/forum/viewtopic.php?f=63&t=12079&p=494488
    # Moved interesting variables to top of script
    # Added options around updates and gzips

--- backup.sh
+++ backup.sh
@@ -12,87 +12,157 @@
 # 2013-Sept-05
 # Remved tar compression, since it takes FOREVER to complete and I don't need it.
 #
+# 2014-Feb-12
+# Merged in comments from http://www.raspberrypi.org/forum/viewtopic.php?f=63&t=12079&p=494488
+# Moved interesting variables to top of script
+# Added options around updates and gzips
+#
 # Add an entry to crontab to run regurlarly.
 # Example: Update /etc/crontab to run backup.sh as root every night at 3am
 # 01 4    * * *   root    /home/pi/scripts/backup.sh
 
+#Setting variables
+DIR=/mnt/fileserver/archive/backups/$(hostname)
+RETENTIONPERIOD=30 # days to keep old backups
+UPDATE=0 # 1 to update packages and firmware
+GZIP=1 # whether to gzip the backup or not
 
-# Setting up directories
-SUBDIR=raspberrypi_backups
-DIR=/mnt/500GB_USB_HD/backups/$SUBDIR
+# Setting up echo fonts
+red='\e[0;31m'
+green='\e[0;32m'
+cyan='\e[0;36m'
+yellow='\e[1;33m'
+purple='\e[0;35m'
+NC='\e[0m' #No Color
+bold=`tput bold`
+normal=`tput sgr0`
 
+LOGFILE="$DIR/backup_$(date +%Y%m%d_%H%M%S).log"
 # Check if backup directory exists
 if [ ! -d "$DIR" ];
    then
-      mkdir -p $DIR
-      echo "Backup directory $DIR doesn't exist, created it now!" >> $DIR/backup.log
+      sudo mkdir $DIR
+	  echo -e "${yellow}${bold}Backup directory $DIR didn't exist, I created it${NC}${normal}"  | tee -a $DIR/backup.log
+      
 fi
 
-echo "____ BACKUP ON $(date +%Y/%m/%d_%H:%M:%S)" >> $DIR/backup.log
-echo "Starting RaspberryPI backup process!" >> $DIR/backup.log
-
+echo -e "${green}${bold}Starting RaspberryPI backup process!${NC}${normal}" | tee -a $DIR/backup.log
+echo "____ BACKUP ON $(date +%Y/%m/%d_%H:%M:%S)" | tee -a $DIR/backup.log
+echo ""
 # First check if pv package is installed, if not, install it first
-if `dpkg -s pv | grep -q Status;`
+PACKAGESTATUS=`dpkg -s pv | grep Status`;
+
+if [[ $PACKAGESTATUS == S* ]]
    then
-      echo "Package 'pv' is installed." >> $DIR/backup.log
+      echo -e "${cyan}${bold}Package 'pv' is installed${NC}${normal}" | tee -a $DIR/backup.log
+      echo ""
    else
-      echo "Package 'pv' is NOT installed." >> $DIR/backup.log
-      echo "Installing package 'pv'. Please wait..." >> $DIR/backup.log
-      apt-get -y install pv
+      echo -e "${yellow}${bold}Package 'pv' is NOT installed${NC}${normal}" | tee -a $DIR/backup.log
+      echo -e "${yellow}${bold}Installing package 'pv' + 'pv dialog'. Please wait...${NC}${normal}" | tee -a $DIR/backup.log
+      echo ""
+      sudo apt-get -y install pv && sudo apt-get -y install pv dialog
 fi
+
 
 
 
 # Create a filename with datestamp for our current backup (without .img suffix)
-OFILE="$DIR/backup_$(date +%Y%m%d_%H%M%S)"
+OFILE="$DIR/backup_$(hostname)_$(date +%Y%m%d_%H%M%S)".img
 
-# Create final filename, with suffix
-OFILEFINAL=$OFILE.img
 
 # First sync disks
 sync; sync
 
 # Shut down some services before starting backup process
-echo "Stopping some services before backup." >> $DIR/backup.log
-nginx -s stop
-service couchdb stop
-service cron stop
+echo ""
+echo -e "${purple}${bold}Stopping services before backup${NC}${normal}" | tee -a $DIR/backup.log
+#sudo pkill deluged
+#sudo pkill deluge-web
+#sudo service deluge-daemon stop
+#sudo service noip stop
+#sudo service proftpd stop
+#sudo service webmin stop
+#sudo service xrdp stop
+#sudo service ddclient stop
+#sudo service apache2 stop
+#sudo service samba stop
+#sudo service avahi-daemon stop
+#sudo service netatalk stop
+#sudo service sendmail stop
+#sudo /var/ossec/bin/ossec-control stop
+#sudo service ssh stop
+sudo service cron stop
 
-# Begin the backup process, should take about 1 hour from 8Gb SD card to HDD
-echo "Backing up SD card to USB HDD." >> $DIR/backup.log
-echo "This will take some time depending on your SD card size and read performance. Please wait..." >> $DIR/backup.log
-SDSIZE=`blockdev --getsize64 /dev/mmcblk0`;
-pv -tpreb /dev/mmcblk0 -s $SDSIZE | dd of=$OFILE bs=1M conv=sync,noerror iflag=fullblock
+
+# Begin the backup process, should take about 45 minutes hour from 8Gb SD card to HDD
+echo ""
+echo -e "${green}${bold}Backing up SD card to img file on HDD${NC}${normal}" | tee -a $DIR/backup.log
+SDSIZE=`sudo blockdev --getsize64 /dev/mmcblk0`;
+if [ $GZIP = 1 ];
+	then
+		echo -e "${green}Gzipping backup${NC}${normal}"
+		sudo pv -tpreb /dev/mmcblk0 -s $SDSIZE | dd  bs=1M conv=sync,noerror iflag=fullblock | gzip > $OFILE.tgz
+	else
+		echo -e "${green}No backup compression${NC}${normal}"
+		sudo pv -tpreb /dev/mmcblk0 -s $SDSIZE | dd of=$OFILE bs=1M conv=sync,noerror iflag=fullblock
+fi
+
 
 # Wait for DD to finish and catch result
 RESULT=$?
 
 # Start services again that where shutdown before backup process
-echo "Start the stopped services again." >> $DIR/backup.log
-service cron start
-service couchdb start
-nginx
+echo ""
+echo -e "${purple}${bold}Starting the stopped services${NC}${normal}" | tee -a $DIR/backup.log
+#sudo service deluge-daemon start
+#sudo deluged
+#sudo deluge-web
+#sudo service noip start
+#sudo service proftpd start
+#sudo service webmin start
+#sudo service xrdp start
+sudo service cron start
 
-# If command has completed successfully, delete previous backups and exit
+
+# If command has completed successfully, if not, delete created files
 if [ $RESULT = 0 ];
    then
-      echo "Successful backup, previous backup files will be deleted." >> $DIR/backup.log
-      rm -f $DIR/backup_*.img
-      mv $OFILE $OFILEFINAL
-      # echo "Backup is being tarred. Please wait..." >> $DIR/backup.log
-      # tar zcf $OFILEFINAL.tar.gz $OFILEFINAL
-      # rm -rf $OFILEFINAL
-      echo "RaspberryPI backup process completed! FILE: $OFILEFINAL" >> $DIR/backup.log
-      echo "____ BACKUP SCRIPT FINISHED $(date +%Y/%m/%d_%H:%M:%S)" >> $DIR/backup.log
-      exit 0
+      echo ""
+      echo -e "${green}${bold}RaspberryPI backup process completed! FILE: $OFILE${NC}${normal}" | tee -a $DIR/backup.log
+      echo -e "${yellow}Removing backups older than $RETENTIONPERIOD days${NC}" | tee -a $DIR/backup.log
+      sudo find $DIR -maxdepth 1 -name "*.img" -mtime +$RETENTIONPERIOD -exec rm {} \;
+      echo -e "${cyan}If any backups older than $RETENTIONPERIOD days were found, they were deleted${NC}" | tee -a $DIR/backup.log
+
+ 
+ 	  if [ $UPDATE = 1 ] ;# Update Raspberry Pi Firmware
+		  then
+				  
+			  # Update Packages and Kernel
+			  echo -e "${yellow}Update Packages and Kernel${NC}${normal}" | tee -a $DIR/backup.log
+			  sudo apt-get update
+			  sudo apt-get upgrade -y
+			  sudo apt-get autoclean
+
+			  echo -e "${yellow}Update Raspberry Pi Firmware${NC}${normal}" | tee -a $DIR/backup.log
+			  sudo rpi-update
+			  sudo ldconfig
+
+			  # Reboot now
+			  echo -e "${yellow}Reboot now ...${NC}${normal}" | tee -a $DIR/backup.log
+			  sudo reboot
+		  else
+			exit 0
+	  fi		
+      
 # Else remove attempted backup file
    else
-      echo "Backup failed! Previous backup files untouched." >> $DIR/backup.log
-      echo "Please check there is sufficient space on the HDD." >> $DIR/backup.log
-      rm -f $OFILE
-      echo "RaspberryPI backup process failed!" >> $DIR/backup.log
-      echo "____ BACKUP SCRIPT FINISHED $(date +%Y/%m/%d_%H:%M:%S)" >> $DIR/backup.log
+      echo ""
+      echo -e "${red}${bold}Backup failed!${NC}${normal}" | tee -a $DIR/backup.log
+     sudo rm -f $OFILE
+      echo ""
+      echo -e "${purple}Last backups on HDD:${NC}" | tee -a $DIR/backup.log
+     sudo find $DIR -maxdepth 1 -name "*.img" -exec ls {} \;
+      echo ""
+      echo -e "${red}${bold}RaspberryPI backup process failed!${NC}${normal}" | tee -a $DIR/backup.log
       exit 1
 fi
-
-
